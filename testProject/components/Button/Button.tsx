a8259e4185dfeb3dddbb227bb232010651029e72
makcbrain <makc.brain@gmail.com>
2025-09-14 12:06:16 +0300

    init storybook with vite

--- testProject/components/Button/Button.tsx
+++
@@ -1,8 +0,0 @@
-export type ButtonProps = {
-    text: string
-    onClick: () => void;
-};
-
-export const Button = ({text, onClick}: ButtonProps) => {
-    return <button onClick={onClick}>{text}</button>
-}
