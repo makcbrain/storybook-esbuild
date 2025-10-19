cf00ab1fc8ebd5e64d54d608a2700e7ff1c5a1e2
makcbrain <makc.brain@gmail.com>
2025-10-19 19:25:03 +0300

    migrate to bun@1.3.0

--- packages/framework-react-esbuild/src/preset.ts
+++ packages/framework-react-esbuild/src/preset.ts
@@ -1,10 +1,15 @@
+import { dirname, join } from 'node:path';
 import type { PresetProperty } from 'storybook/internal/types';
 
 import type { StorybookConfig } from './types.ts';
 
+const getAbsolutePath = (value: string): string => {
+	return dirname(require.resolve(join(value, 'package.json')));
+};
+
 export const core: PresetProperty<'core'> = {
-	builder: '@makcbrain/storybook-builder-esbuild',
-	renderer: '@storybook/react/preset',
+	builder: getAbsolutePath('@makcbrain/storybook-builder-esbuild'),
+	renderer: getAbsolutePath('@storybook/react'),
 };
 
 export const esbuildFinal: StorybookConfig['esbuildFinal'] = async (config) => {
