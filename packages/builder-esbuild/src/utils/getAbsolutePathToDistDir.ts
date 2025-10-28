29620060e103f59526a29c783091f431e58dc14e
makcbrain <makc.brain@gmail.com>
2025-10-28 22:02:41 +0200

    use node_modules/.cache dir for built files

--- packages/builder-esbuild/src/utils/getAbsolutePathToDistDir.ts
+++ packages/builder-esbuild/src/utils/getAbsolutePathToDistDir.ts
@@ -1,8 +1,24 @@
 import { join } from 'node:path';
+import { cache } from 'empathic/package';
 import type { Options } from 'storybook/internal/types';
 
 import { DIST_DIR_NAME } from '../constants.js';
 
 export const getAbsolutePathToDistDir = (options: Options): string => {
+	const cacheDir = cache(`@makcbrain-storybook-builder-esbuild`, {
+		create: true,
+		cwd: options.configDir,
+	});
+
+	if (cacheDir) {
+		return cacheDir;
+	}
+
+	console.warn(
+		'[@makcbrain/storybook-builder-esbuild] Could not find or create cache directory in node_modules/.cache. ' +
+			`Using ${DIST_DIR_NAME} directory in .storybook instead. ` +
+			'Please ensure this directory is added to .gitignore and linter exclusions.',
+	);
+
 	return join(options.configDir, DIST_DIR_NAME);
 };
